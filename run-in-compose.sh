321aebfe2c763de5ea0908342527080ce9925ebe
Mikhail Petrusheuski <mikhail.petrusheuski@covergo.com>
2024-02-15 14:30:56 +0300

    PLATFORM-875: Added opportunity to add environmental variables

--- run-in-compose.sh
+++ run-in-compose.sh
@@ -1,6 +1,10 @@
 service=$1
 project=${2:-"run-in-compose"}
 compose_file=${3:-"docker-compose.yml"}
+env_vars=${4:-"{}"}
+
+echo "$env_vars" | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' | while read var; do export "$var"; done
+
 docker compose --file "$compose_file" pull -q
 docker compose --file "$compose_file" -p "$project" run $service
 exit $?
